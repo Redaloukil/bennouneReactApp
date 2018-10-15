aba68efef3674ce480624787bcbdeffc01a3eb38
RedaLoukil <you@example.com>
2018-10-15 17:47:00 +0100

    add login and signup

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link} from 'react-router-dom';
 
 
+
 class Navbar extends React.Component {
     render(){
         return(
@@ -9,7 +10,7 @@ class Navbar extends React.Component {
             
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <a className="navbar-brand" href="#">Hajala</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
@@ -17,24 +18,14 @@ class Navbar extends React.Component {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item active">
-                  <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                  <a className="nav-link" href="#"><Link to='/'>Bennoune</Link></a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Link</a>
+                  <a className="nav-link"><Link to='/login'>Login</Link></a>
                 </li>
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                  </a>
-                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#">Something else here</a>
-                  </div>
-                </li>
+                
                 <li className="nav-item">
-                  <a className="nav-link disabled" href="#">Disabled</a>
+                  <a className="nav-link" href="#"><Link to='/signup'>Signup</Link></a>
                 </li>
               </ul>
               
--- src/app/screens/Home.js
+++ src/app/screens/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../../App.css';
 
+
+
 const Home = () => {
     return(
         <header>
@@ -9,7 +11,48 @@ const Home = () => {
             <div id="description">
                 <h1>Bennoune is expensive this days</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda suscipit consectetur explicabo blanditiis ut laboriosam, est provident, enim quos optio laudantium ducimus error tenetur veniam perferendis pariatur facere, quidem doloremque!</p>
+            
             </div>
+            <br/>
+            
+            <div id="search-bennoune">
+                <form class="form-inline col-sm-12">
+                <button class="btn btn-outline-success button-search col-sm-2" type="submit">Search</button>
+                <input class="form-control input-search col-sm-10" type="search" placeholder="new poeple are in the stock.." aria-label="Search"/>
+                </form>    
+            </div>
+            <br/><br/><br/><br/><br/><br/>
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="promo">
+                    <i class="fa fa-bolt" aria-hidden="true"></i>
+                    <p>hello</p>
+                    </div>
+                    
+                </div>
+                <div class="col-sm-3">
+                    <div class="promo">
+                    <i class="fa fa-camera-retro" aria-hidden="true"></i>
+                    <p>hello</p>
+                    </div>
+                    
+                </div>
+                <div class="col-sm-3">
+                    <div class="promo">
+                    <i class="fa fa-female" aria-hidden="true"></i>
+                    <p>hello</p>
+                    </div>
+                    
+                </div>
+                <div class="col-sm-3">
+                    <div class="promo">
+                    <i class="fa fa-leaf" aria-hidden="true"></i>
+                    <p>hello</p>
+                    </div>
+                    
+                </div>
+            </div>
+
         </div>
     </div>
         </header>
